f90cd5b492a62d389c4a8e82fb7a08a4085d4bd9
Alex Jolovic <ajolovic@me.com>
2018-03-20 07:29:25 -0700

    fix: Change class component to function

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,18 @@
 import React, {Component} from "react"
 import { Link } from "react-router-dom"
 
-class Search extends Component {
-  render() {
-    return (
-      <div className="search-books-bar">
-        <Link to="/" className="close-search">Close</Link>
-        <div className="search-books-input-wrapper">
-        <input type="text"
-            placeholder="Search by title or author"
-            onChange={(e) => this.props.appInterface.search.onQueryChange(e.target.value.trim())}
-            value={this.props.appInterface.search.getQuery()} />
-        </div>
+const Search = (props) => {
+  return (
+    <div className="search-books-bar">
+      <Link to="/" className="close-search">Close</Link>
+      <div className="search-books-input-wrapper">
+      <input type="text"
+          placeholder="Search by title or author"
+          onChange={(e) => props.appInterface.search.onQueryChange(e.target.value.trim())}
+          value={props.appInterface.search.getQuery()} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Search
